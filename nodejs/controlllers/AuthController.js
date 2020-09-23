21edb13f8192a835c1bfe96dded22910f27cee13
XinmanZ <zhangxinman@zhangximandeMBP.attlocal.net>
2020-09-23 10:53:20 -0400

    update login

--- nodejs/controlllers/AuthController.js
+++ nodejs/controlllers/AuthController.js
@@ -1,6 +1,7 @@
-const User = require('../models/User');
+const {User, validate} = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const register = (req, res, next) => {
     bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
@@ -28,10 +29,12 @@ const register = (req, res, next) => {
 };
 
 const login = (req, res, next) => {
-    let username = req.body.username;
+    //let username = req.body.username;
+    let email = req.body.email;
     let password = req.body.password;
 
-    User.findOne({$or: [{email: username}, {phone: username}]})
+    //User.findOne({$or: [{email: username}, {phone: username}]})
+    User.findOne({email: email})
         .then(user => {
             if (user) {
                 bcrypt.compare(password, user.password, function (err, result) {
