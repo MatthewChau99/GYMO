13ffddfc061f4fb8ee4065a4e3187454a1b52f18
xfy990107 <1009845608@qq.com>
2020-11-04 13:13:41 +0800

    merge with backend-mat

--- nodejs/routes/posts.js
+++ nodejs/routes/posts.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const Post = require('../models/Posts');
-
-//get all the posts
-router.get('/', async (req, res) => {
-    try{
-        const posts = await Post.find();
-        res.json(posts);
-    } catch(err) {
-        res.json({message: err});
-    }
-});
-=======
 const PostController = require('../controllers/PostController');
->>>>>>> 03aa0f98538517a223bc31bfc74722ee62cd2efb
 
 //submit a post
 router.post('/submitPost', PostController.uploadPost);
@@ -25,18 +11,7 @@ router.get('/:postID', PostController.getPostById);
 
 
 //delete a specific post
-<<<<<<< HEAD
-router.delete('/:postID', async (req, res) => {
-    try{
-         const removedPost = await Post.remove({_id: req.params.postID});
-         res.json(removedPost);
-    } catch(err) {
-        res.json({message:err});
-    }
-});
-=======
 router.delete('/:postID', PostController.deletePost);
->>>>>>> 03aa0f98538517a223bc31bfc74722ee62cd2efb
 
 //update a specific post
 router.patch('/:postID', PostController.updatePost);
